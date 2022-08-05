053aa615ee0d63beb74bc9b49e0b3f7a4255050a
Mateus <creztfallen@M.S.M>
2022-08-05 16:12:51 -0300

    reviewing basic http

--- webModule/server.js
+++ webModule/server.js
@@ -1,2 +1,29 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
+// Create a server
+http
+  .createServer(function (req, res) {
+    // Parse the request containing file name
+    var pathname = '/'
+
+    // Print the name of the file for which request is made.
+    console.log('Request for ' + pathname + ' received.');
+    console.log(pathname)
+    // Read the requested file content from file system
+    fs.readFile("index.html", (err, data) => {
+      if (err) {
+        console.error(err);
+
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data.toString());
+      }
+      res.end();
+    });
+  })
+  .listen(3000);
+
+console.log('Server running on port 3000');
