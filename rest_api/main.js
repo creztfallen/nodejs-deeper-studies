85c42adc563e89c1ad4dfd72b7ee918cd5a8ffc7
Mateus <creztfallen@M.S.M>
2022-08-09 13:44:31 -0300

    first endpoint

--- rest_api/main.js
+++ rest_api/main.js
@@ -7,8 +7,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-const server = app.listen(3000, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log('App listening at http://%s:%s', host, port);
+app.get('/listUsers', (req, res) => {
+  fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
+    res.end(data);
   });
+});
+
+const server = app.listen(3000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('App listening at http://%s:%s', host, port);
+});
