1eb0372054921c658e99c67e89587847c5bd2a49
Mateus <creztfallen@M.S.M>
2022-08-10 09:20:37 -0300

    rest api

--- rest_api/main.js
+++ rest_api/main.js
@@ -4,12 +4,37 @@ const fs = require('fs');
 
 const app = express();
 
+const user = {
+  user4: {
+    name: 'yennefer',
+    password: 'password4',
+    profession: 'sorceress',
+    id: 4,
+  },
+};
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/listUsers', (req, res) => {
   fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
     res.end(data);
+  });
+});
+
+app.post('/addUser', (req, res) => {
+  fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
+    data = JSON.parse(data);
+    data['user4'] = user['user4'];
+    res.end(JSON.stringify(data));
+  });
+});
+
+app.get('/:id', (req, res) => {
+  fs.readFile(__dirname + '/' + 'users.json', 'utf8', function (err, data) {
+    const users = JSON.parse(data);
+    const user = users['user' + req.params.id];
+    res.end(JSON.stringify(user));
   });
 });
 
